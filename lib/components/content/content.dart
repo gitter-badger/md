5af277c7f0619ed90953e796d4aa787e99d58fe8
Adão Júnior <itsjunnior@gmail.com>
2016-05-08 23:14:29 -0300

    feat(MdContent) (#8)

---
+++ lib/components/content/content.dart
@@ -0,0 +1,21 @@
+import "package:angular2/core.dart" show Directive;
+
+/**
+ * @name mdContent
+ *
+ * 
+ * The `<md-content>` directive is a container element useful for scrollable content
+ *
+ * @usage
+ *
+ * - Add the `[layout-padding]` attribute to make the content padded.
+ *
+ * <hljs lang="html">
+ *  <md-content layout-padding>
+ *      Lorem ipsum dolor sit amet, ne quod novum mei.
+ *  </md-content>
+ * </hljs>
+ *
+ */
+@Directive(selector: "md-content")
+class MdContent {}
